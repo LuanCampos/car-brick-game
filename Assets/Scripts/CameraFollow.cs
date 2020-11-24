cf9612da063f2f35e3ecd21cf34164071401e4bf
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2020-11-24 10:49:16 -0300

    Add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	private Transform player;
+	
+	void Start()
+	{
+		player = GameObject.Find("Player").GetComponent<Transform>();
+	}
+	
+    void LateUpdate()
+    {
+        transform.position = new Vector3(0f, player.position.y + 4f, -10f);
+    }
+}
