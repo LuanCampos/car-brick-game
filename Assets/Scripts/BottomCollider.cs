e40b65c6af5f3fbd05eb9e9f091ca979ad4a1d03
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2020-11-24 10:59:32 -0300

    Add bottom collider

---
+++ Assets/Scripts/BottomCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomCollider : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+	{
+		Destroy(col.gameObject);
+	}
+}
