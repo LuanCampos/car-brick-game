c2c76280b8adda5af7b853d99abbd4fa96eedc88
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2020-11-24 09:51:24 -0300

    Add car prefab and input variable

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private float moveInput = 0f;
+	
+	void Update()
+	{
+		moveInput = Input.GetAxis("Horizontal");
+	}
+	
+	void FixedUpdate()
+	{
+		if (moveInput > 0)
+		{
+			Debug.Log("MoveInput maior do que zero");
+		}
+		
+		if (moveInput < 0)
+		{
+			Debug.Log("MoveInput menor do que zero");
+		}
+	}
+}
