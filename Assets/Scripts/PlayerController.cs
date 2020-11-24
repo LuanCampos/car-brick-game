8fa16746d3618e0335e575effb0bf9aaa1307f9a
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2020-11-24 10:42:56 -0300

    Add movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
 	private float moveInput = 0f;
 	private float step = 1.8f;
+	private int frameCount = 10;
+	private int velocity = 1;
 	private bool isMoving = false;
+	private Vector3 movement;
 	
 	void Update()
 	{
@@ -24,16 +27,27 @@ public class PlayerController : MonoBehaviour
 	
 	void FixedUpdate()
 	{
+		movement = Vector3.zero;
+		
+		if (frameCount - velocity <= 0)
+		{
+			movement.y = .6f;
+			frameCount = 10;
+		}
+		
 		if (moveInput > 0f && transform.position.x < step)
 		{
-			transform.position = new Vector3 (transform.position.x + step, transform.position.y, 0f);
+			movement.x += step;
 			moveInput = 0f;
 		}
 		
 		if (moveInput < 0f && transform.position.x > -step)
 		{
-			transform.position = new Vector3 (transform.position.x - step, transform.position.y, 0f);
+			movement.x -= step;
 			moveInput = 0f;
 		}
+		
+		transform.position += movement;
+		frameCount --;
 	}
 }
